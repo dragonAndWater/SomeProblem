132890b91db99fc6145aa4f0f18a016fb6fe12c1
longtao <long_tao@aliyun.com>
2020-04-20 17:44:36 +0800

    --新增jsonFormat工具，可以自动将json转化为model，快捷键：alt+s

---
+++ src/JsonModelTest.java
@@ -0,0 +1,26 @@
+public class JsonModelTest {
+
+    /**
+     * a : Hello
+     * b : World
+     */
+
+    private String a;
+    private String b;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
